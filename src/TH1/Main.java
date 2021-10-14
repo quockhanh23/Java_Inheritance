8ed488d35e1c79e55e28c1058971f9d2121b7ac7
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-10-14 15:33:46 +0700

    Bai6_kethua

---
+++ src/TH1/Main.java
@@ -0,0 +1,25 @@
+package TH1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle(3.5, "indigo", false);
+        System.out.println(circle);
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+
+    }
+}
+
